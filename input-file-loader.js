35e9ef7f14c4122880ab72de2dd710a989b69e9e
Pudding.Tbook16 <pulipuli.chen@gmail.com>
2017-08-25 15:18:39 +0800

    0825 開始處理檔案讀取的問題

---
+++ input-file-loader.js
@@ -0,0 +1,7 @@
+// <input type="file" class="input-field" id="input_csv_file" load_content_to="#inpu_csv_file_textarea" />
+
+$(function () {
+    $("input:file[load_content_to]").change(function () {
+        
+    });
+});
